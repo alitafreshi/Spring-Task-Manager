f6736933d1b3a66b9a37d71d0d8a52a96996bb9f
a.tafreshi <alitafreshi123@yahoo.com>
2024-01-01 11:32:41 +0330

    User Crud Service  Layer Functionality

--- src/main/java/com/alitafreshi/task_manager/task/service/TaskService.java
+++ src/main/java/com/alitafreshi/task_manager/task/service/TaskService.java
@@ -1,7 +1,10 @@
 package com.alitafreshi.task_manager.task.service;
 
+import com.alitafreshi.task_manager.task.model.Task;
 import com.alitafreshi.task_manager.task.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -10,6 +13,17 @@ public class TaskService {
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public void insertNewTask(Task newTask){
+        taskRepository.save(newTask);
+    }
+
+    public List<Task> getAllTaskListByUserId(Long userId){
+        return taskRepository.findByUserId(userId);
+    }
+    public void deletedByTaskId(Long taskId){
+        taskRepository.deleteById(taskId);
     }
 
 }
--- src/main/java/com/alitafreshi/task_manager/user/service/UserService.java
+++ src/main/java/com/alitafreshi/task_manager/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.alitafreshi.task_manager.user.service;
 
+import com.alitafreshi.task_manager.user.model.User;
 import com.alitafreshi.task_manager.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,6 +17,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public void insertNewUser(User newUser) {
+        userRepository.save(newUser);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long userId) {
+        boolean exists = userRepository.existsById(userId);
+        if (!exists) {
+            throw new IllegalStateException("user with id" + userId + "dose not exist");
+        }
+        userRepository.deleteById(userId);
+    }
 
 
 }
