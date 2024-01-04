0e2d9b66c2031be5ef10755f20af635aa8f8cfa7
alitafreshi <alitafreshi123@yahoo.com>
2024-01-04 21:24:33 +0330

    Add user crud functionality

--- src/main/java/com/alitafreshi/task_manager/user/service/UserService.java
+++ src/main/java/com/alitafreshi/task_manager/user/service/UserService.java
@@ -2,17 +2,13 @@ package com.alitafreshi.task_manager.user.service;
 
 import com.alitafreshi.task_manager.user.model.User;
 import com.alitafreshi.task_manager.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -21,18 +17,12 @@ public class UserService {
         userRepository.save(newUser);
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public void deleteUser(User user) {
+        userRepository.delete(user);
     }
 
-    public void deleteUser(Long userId) {
-        boolean exists = userRepository.existsById(userId);
-        if (!exists) {
-            throw new IllegalStateException("user with id" + userId + "dose not exist");
-        }
-        userRepository.deleteById(userId);
+    public void findUserByPhoneNumber(String phoneNumber){
+       userRepository.findUserByPhoneNumber(phoneNumber);
     }
-
 
 }
-
